2ffcfe9c1c4c4663b557b0fe87db17b2223a10a9
uranium93 <19hanafi93@gmail.com>
2019-09-05 20:09:09 +0100

    redux actionstype

---
+++ client/src/store/actions/actionTypes.js
@@ -0,0 +1,19 @@
+export const SET_ALERT = "SET_ALERT";
+export const REFRESH_TOKEN = "REFRESH_TOKEN"
+
+
+// User 
+export const REGISTER_SUCC = "REGISTER_SUCC"
+export const REGISTER_FAIL = "REGISTER_FAIL"
+export const REGISTER_START = "REGISTER_START"
+
+export const LOGIN_START = "LOGIN_START"
+export const LOGIN_SUCC = "LOGIN_SUCC"
+export const LOGIN_FAIL = "LOGIN_FAIL"
+
+export const LOGOUT = "LOGOUT"
+
+// Locations
+export const ADD_LOCATION_START = "ADD_LOCATION_START"
+export const ADD_LOCATION_SUCC = "ADD_LOCATION_SUCC"
+export const ADD_LOCATION_FAIL = "ADD_LOCATION_FAIL"
